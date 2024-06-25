52dcc13d6c7185608b9e847e901edaf266108300
SEEN88npdc <seancurtinnpdc@gmail.com>
2024-06-26 10:44:30 +1200

    Create allinone.ps1

---
+++ allinone.ps1
@@ -0,0 +1,2 @@
+Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+winget install -s winget --accept-package-agreements --accept-source-agreements --id Adobe.Acrobat.Reader.64-bit
